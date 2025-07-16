fd57a4bf47d5bf2764a5b044d915f65f38757637
Maidamsheetyvivek <vivekmaidamsheety@gmail.com>
2025-07-16 12:13:57 +0530

    Modified the file

--- src/loginModule/HealthCareLogin.java
+++ src/loginModule/HealthCareLogin.java
@@ -5,6 +5,8 @@ public class HealthCareLogin {
 	public static void main(String[] args) {
 		System.out.println("UserName");
 		System.out.println("Password");
+		System.out.println("Click on Submit button");
+		
 
 	}
 
